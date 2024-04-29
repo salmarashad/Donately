a0032f7317d1f3867cc17355b8f38ddcc45497ec
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-04-30 00:02:58 +0300

    Added Notification Button - missing actual cards

--- ch-and-co-m2/src/App.js
+++ ch-and-co-m2/src/App.js
@@ -1,22 +1,7 @@
 import './App.css';
+import NotificationComponent from './Components/NotificationComponent';
 
 function App() {
-  const Bell = () => {
-    return (
-      <div className="mx-auto">
-        <svg fill="#333333" height="27px" width="27px" viewBox="0 0 448 512">
-          <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
-          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
-          <g id="SVGRepo_iconCarrier">
-            <g>
-              <path d="M222.987,510c31.418,0,57.529-22.646,62.949-52.5H160.038C165.458,487.354,191.569,510,222.987,510z"/>
-              <path d="M432.871,352.059c-22.25-22.25-49.884-32.941-49.884-141.059c0-79.394-57.831-145.269-133.663-157.83h-4.141 c4.833-5.322,7.779-12.389,7.779-20.145c0-16.555-13.42-29.975-29.975-29.975s-29.975,13.42-29.975,29.975 c0,7.755,2.946,14.823,7.779,20.145h-4.141C120.818,65.731,62.987,131.606,62.987,211c0,108.118-27.643,118.809-49.893,141.059 C-17.055,382.208,4.312,434,47.035,434H398.93C441.568,434,463.081,382.269,432.871,352.059z"/>
-            </g>
-          </g>
-        </svg>
-      </div>
-    );
-  };
   
   const Profile = () => {
     return (
@@ -35,7 +20,7 @@ function App() {
   return (
     <div className="min-h-screen">
       {/* grid splits child divs into a grid of size 3 */}
-      <div className="grid grid-cols-3 mx-64 items-center h-16">
+      <div className="grid grid-cols-3 px-64 items-center h-16 shadow-md">
         {/* logo div */}
         <div>
           {/* Temporary text, replace with real logo yara */}
@@ -49,7 +34,7 @@ function App() {
         </div>
         {/* Notifications, Profile, Logout, also a grid of three */}
         <div className="grid grid-cols-3 font-medium text-right ml-64 items-center justify-center">
-          <Bell></Bell>
+          <NotificationComponent/>
           <Profile></Profile>
           <h1>Logout</h1>
         </div>
