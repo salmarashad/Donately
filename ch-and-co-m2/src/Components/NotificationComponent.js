c15ef53de608340fce099192346d7e93da4d0450
Farah065 <farahahmad065@gmail.com>
2024-05-02 20:43:51 +0300

    centred the notif popup

--- ch-and-co-m2/src/Components/NotificationComponent.js
+++ ch-and-co-m2/src/Components/NotificationComponent.js
@@ -8,11 +8,15 @@ function NotificationComponent() {
             <div onClick={() => setIsClicked(!isClicked)}>
                 <NotifSVG className="h-6 w-7" />
             </div>
-            <div className={isClicked === false ? "hidden" : "absolute top-12 h-72 w-72 -right-20 shadow-lg bg-white rounded-md"}>
-                <div className='text-center items-center'>
-                    <h1 className='mt-2'>Notifications</h1>
+            <div className={isClicked === false ? "hidden" : "block"}>
+                {/* little triangle above the notif container */}
+                <div className="absolute top-10 h-8 w-8 -right-0.5 bg-white shadow-lg rotate-45" />
+                {/* actual notif container */}
+                <div className="absolute top-12 h-72 w-72 -right-32 shadow-lg bg-white rounded-md">
+                    <div className='text-center items-center'>
+                        <h1 className='mt-2'>Notifications</h1>
+                    </div>
                 </div>
-                
             </div>
         </div>
     );
