e26c41a121792d6272d9684d1b8cb376485de5bd
Ray <142018929+YaraTantawy@users.noreply.github.com>
2024-05-04 02:00:58 +0300

    farah-colored

--- ch-and-co-m2/src/Components/Registration.js
+++ ch-and-co-m2/src/Components/Registration.js
@@ -5,41 +5,41 @@ function Registration({setPage}) {
             {/*organization vs user registration */}
             <div>
 
-                <div className='flex justify-center h-2/4'>
-                <div>
-                    <img src= {process.env.PUBLIC_URL + 'login.png'} className=' max-w-lg'/>
-                </div>
-                <div className=' flex flex-col h-50 justify-center '>
-                    <p className='text-left text-5xl text-green-800 font-semibold font-sans '>
-                        Welcome to our website!
-                    </p>
-                    <p className='text-center text-xl text-green-600 font-sans pt-5'>
-                        Signing in for the first time? Let us get to know you!
-                    </p>
-                    
-                    <div className='flex flex-row justify-around pt-16'>
-                        {/*organization*/}
-                        <button 
-                            className=" bg-lime-800 hover:bg-lime-900 text-slate-100 text-2xl rounded-2xl flex-shrink-0 p-4 min-w-44"
-                        >
-                            Organization
-                        </button>
-
-                        {/*User*/}
-                        <button 
-                            className="bg-lime-800 hover:bg-lime-900 text-slate-100 text-2xl rounded-2xl flex-shrink-0 p-4 min-w-44"
-                        >
-                            User
-                        </button>
+                <div className='flex justify-center'>
+                    <div>
+                        <img src= {process.env.PUBLIC_URL + 'login.png'} className=' max-w-lg'/>
                     </div>
+                    <div className=' flex flex-col h-50 justify-center bg-white border-2 border-dotted border-farahgreen-200 rounded-2xl p-8'>
+                        <p className='text-left text-5xl text-farahgreen-300 font-semibold font-sans '>
+                            Welcome to our website!
+                        </p>
+                        <p className='text-center text-xl text-farahgreen-300 font-sans pt-5'>
+                            Signing in for the first time? Let us get to know you :]
+                        </p>
+                        
+                        <div className='flex flex-row justify-center space-x-10 pt-16'>
+                            {/*organization*/}
+                            <button 
+                                className=" bg-farahgray-200 hover:bg-farahgreen-200 text-slate-600 text-2xl rounded-2xl flex-shrink-0 p-4 min-w-44"
+                            >
+                                Organization
+                            </button>
 
-                    {/*Redirect*/}
-                    <p className='text-center text-green-600 text-lg underline pt-6 cursor-pointer' 
-                    onClick={() => setPage("login")}>
-                        Returning user? log in here.
-                    </p>
+                            {/*User*/}
+                            <button 
+                                className=" bg-farahgray-200 hover:bg-farahgreen-200 text-slate-600 text-2xl rounded-2xl flex-shrink-0 p-4 min-w-44"
+                            >
+                                User
+                            </button>
+                        </div>
 
-                </div>
+                        {/*Redirect*/}
+                        <p className='text-center text-green-600 text-lg underline pt-6 cursor-pointer' 
+                        onClick={() => setPage("login")}>
+                            Returning user? log in here.
+                        </p>
+
+                    </div>
                 </div>
 
             </div>
