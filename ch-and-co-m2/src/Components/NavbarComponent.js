8a842471450a9aaa1a84ff2f165b1b01e930cf62
Farah065 <farahahmad065@gmail.com>
2024-05-04 14:26:46 +0300

    fixed navbar (again)

--- ch-and-co-m2/src/Components/NavbarComponent.js
+++ ch-and-co-m2/src/Components/NavbarComponent.js
@@ -34,22 +34,16 @@ function NavbarComponent( {setPage} ) {
 						<h1>Organizations</h1>
 					</button>
 					{/* Notifications, Profile, Logout, also a flexbox */}
-					<div className="flex font-medium items-center justify-center gap-4">
+				</div>
+				<div className="flex font-medium items-center justify-center gap-4">
 						<NotificationComponent />
 						<ProfileSVG className="h-7 w-7" />
-						<button
-						className="hover:bg-slate-100 cursor-pointer"
-						onClick={() => setPage("registration")}
-						>
-						<h1>Register</h1>
-						</button>
 						<button 
-						className="hover:bg-slate-100 cursor-pointer"
-						onClick={() => setPage("login")}>
+						className="cursor-pointer nav-item"
+						onClick={() => setPage("registration")}>
 						<h1>Log in</h1>
 						</button>
 					</div>
-				</div>
 			</div>
 		</div>
     );
