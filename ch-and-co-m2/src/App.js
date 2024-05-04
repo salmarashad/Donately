041c44ca33c10b6e70fc13a82c66aaadea87b540
Ray <142018929+YaraTantawy@users.noreply.github.com>
2024-05-04 17:37:52 +0300

    login re-re-design

--- ch-and-co-m2/src/App.js
+++ ch-and-co-m2/src/App.js
@@ -15,7 +15,7 @@ function App() {
 		<div className="h-screen font-karla text-farahblack">
 			<NavbarComponent setPage={setPage} isLoggedin = {isLoggedin} setIsLoggedin={setIsLoggedin} />
 			{/* rest of the body */}
-			<div className="bg-gray-200 min-h-full pb-8 pt-24">
+			<div className="bg-gray-200 h-full pb-8 pt-24">
 				{page === "donations" && <DonationsView />}
 				{page === "volunteering" && <h1>Volunteering</h1>}
 				{page === "organizations" && <h1>Organizations</h1>}
--- ch-and-co-m2/src/Components/Login.js
+++ ch-and-co-m2/src/Components/Login.js
@@ -1,23 +1,24 @@
 import React, { useState } from 'react';
 function Login( props) { 
     return(
-        <div>
-            <form className=' flex flex-col space-y-5 items-center justify-center h-96'> 
-            <label className='flex flex-col text-lg font-semibold font-sans text-farahgreen-300'>
-                Userame:
-                <input type="text" name="name" className=" rounded-lg p-1 pr-10 my-2"/>
-            </label>
-            <label className='flex flex-col text-lg font-semibold font-sans text-farahgreen-300'>
-                Password:
-                <input type="password" name="name" className=" rounded-lg p-1 pr-10" />
-            </label>
-            <input type="submit" value="Submit" className=' cursor-pointer bg-farahgray-200 hover:bg-farahgreen-200 text-lg font-semibold p-1 px-2 rounded-xl border-2 border-solid border-farahgreen-200' />
-            
-            {/*Redirect*/}
-            <p className='text-center text-green-600 text-lg underline cursor-pointer' 
-                        onClick={() => props.setPage("registration")}>
-                            New user? Create an account 
-                        </p>
+        <div className=' h-full grid items-center justify-center'>
+            <form className=' flex flex-col space-y-5 items-center justify-center h-96  content-center bg-farahgreen-200 p-8 rounded-xl mb-16'> 
+                    <label className='flex flex-col text-lg font-semibold text-farahgreen-300'>
+                        Userame:
+                        <input type="text" name="name" className=" rounded-lg p-1 pr-10 my-2"/>
+                    </label>
+                    <label className='flex flex-col text-lg font-semibold text-farahgreen-300'>
+                        Password:
+                        <input type="password" name="name" className=" rounded-lg p-1 pr-10 my-2" />
+                    </label>
+                    <input type="submit" value="Submit" 
+                    className=' cursor-pointer bg-farahgray hover:bg-farahgreen-300 text-lg text-slate-200 font-semibold p-1 px-2 rounded-xl border-2 border-solid border-farahgreen-200' />
+                    
+                    {/*Redirect*/}
+                    <p className='text-center text-farahgreen-300 text-lg underline cursor-pointer' 
+                                onClick={() => props.setPage("registration")}>
+                                    New user? Create an account 
+                                </p>
 
             </form>
 
