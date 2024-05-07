bd004efdeec69472c66a698719fd78e962e0b701
Ray <142018929+YaraTantawy@users.noreply.github.com>
2024-05-07 22:27:01 +0300

    may fuck shit up precautionary push <3

--- ch-and-co-m2/src/Components/Maps.js
+++ ch-and-co-m2/src/Components/Maps.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from 'google-map-react';
 import {ReactComponent as Pin} from '../SVGs/pin.svg';
+import { App } from "../App";
 
 
-
-
-export default function Maps(){
+export default function Maps(props){
 
   const [currlocation, setCurrLocation] = React.useState([0,0]);
   const [finalLocation, setFinalLocation] = React.useState([0,0]);
@@ -17,6 +16,20 @@ export default function Maps(){
     },
     zoom: 11
   };
+
+  function randomizeL(){
+    var long = Math.round((Math.random()*(30-32)+32) * 1000000)/1000000
+    var lat = Math.round((Math.random()*(31-28)+28) * 1000000)/1000000
+    console.log(long)
+    console.log(lat)
+    setFinalLocation([lat,long])
+  }
+
+  useEffect(()=>{
+    if (props.isStaticMap){
+      randomizeL();
+    }
+  },[])
 
   function onBoundsChange(bounds){
     setCurrLocation(bounds);
@@ -33,14 +46,21 @@ export default function Maps(){
         defaultZoom={defaultProps.zoom}
         onBoundsChange={onBoundsChange}
       >
-        
-        {finalLocation[0] !== 0 && finalLocation[1] !== 0 && <AnyReactComponent
-          lat={finalLocation[0]}
-          lng={finalLocation[1]}
-          text="My Location"
-        />}
+        {props.isStaticMap?
+          <div>
+            {finalLocation[0] !== 0 && finalLocation[1] !== 0 && <AnyReactComponent
+              lat={finalLocation[0]}
+              lng={finalLocation[1]}
+              text="My Location"
+            />}
+          </div>
+        :
+        <div>
+          
+        </div>
+        }
       </GoogleMapReact>
-      <div className="absolute top-44 left-44">
+      {props.isStaticMap === false && <div className="absolute top-44 left-44">
         <Pin/>
         <button
         onClick={onClick}
@@ -48,7 +68,7 @@ export default function Maps(){
         >
         Set Location
         </button>
-      </div>
+      </div>}
       <div className="absolute top-12 left-12">
         
       </div>
