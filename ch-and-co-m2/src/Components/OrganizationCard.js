a5c1cc9a85281c813b37268060d5b055ac454d17
salmarashad <salmamarashad@gmail.com>
2024-05-08 19:02:52 +0300

    fixed layout a bit (org)

--- ch-and-co-m2/src/Components/OrganizationCard.js
+++ ch-and-co-m2/src/Components/OrganizationCard.js
@@ -28,7 +28,7 @@ const OrganizationCard = ({ organization }) => {
     ));
   };
 
-  const { name, type, imgURL, tags } = organization;
+  const { title, subtitle, imgURL, tags } = organization;
 
   return (
     <div className="organization-card bg-white shadow-md rounded-lg overflow-hidden flex flex-col justify-between h-full p-4 ">
@@ -36,19 +36,19 @@ const OrganizationCard = ({ organization }) => {
         {imgURL && (
           <img
             src={imgURL}
-            alt={name}
+            alt={title}
             className="w-32 h-32 object-cover rounded-md mb-4 "
             style={{ borderRadius: "8px" }}
           />
         )}
       </div>
       <div className="flex justify-center my-1">
-        <p className="text-lg text-center font-semibold">{name}</p>
+        <p className="text-lg text-center font-semibold">{title}</p>
       </div>
       <div className="flex justify-center mt-1 mb-4">
-        <p className="text-sm text-gray-500">{type}</p>
+        <p className="text-sm text-gray-500">{subtitle}</p>
       </div>
-      <div className="flex justify-center my-2">{renderTags(tags)}</div>
+      {/* <div className="flex justify-center my-2">{renderTags(tags)}</div> */}
       <div className="flex justify-center my-2">
         <button className="text-sm italic border border-farahgreen-300 text-farahgreen-300 px-4 py-1 rounded-xl cursor-pointer mb-6">
           View Details {">"}
