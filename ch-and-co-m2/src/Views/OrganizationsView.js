f0d7b4cbadf3c9ae89aa9083d6791f68d941f1bc
salmarashad <salmamarashad@gmail.com>
2024-05-08 20:05:10 +0300

    added all types of examples

--- ch-and-co-m2/src/Views/OrganizationsView.js
+++ ch-and-co-m2/src/Views/OrganizationsView.js
@@ -54,39 +54,51 @@ const OrganizationsView = () => {
     },
     {
       title: "Resala",
-      subtitle: "Charity",
+      subtitle: "Non-profit",
       description:
         "Resala is a charitable organization dedicated to helping those in need through various initiatives.",
       tags: {
         governorate: "Cairo",
         area: "Maadi",
-        type: "Charity",
+        type: "Non-profit",
       },
       imgURL: `${process.env.PUBLIC_URL}/organizationImages/Resala.png`,
     },
     {
-      title: "Resala",
-      subtitle: "Charity",
+      title: "Awlady Orphanage",
+      subtitle: "Orphanage",
       description:
-        "Resala is a charitable organization dedicated to helping those in need through various initiatives.",
+        "Awlady Orphanage is a home for orphaned children, providing them with shelter, care, and support. It aims to create a nurturing environment where children can thrive and grow into confident individuals. Through its dedicated staff and volunteers, Awlady Orphanage offers a range of services and programs to meet the physical, emotional, and educational needs of the children under its care.",
       tags: {
         governorate: "Cairo",
         area: "Zamalek",
-        type: "Charity",
+        type: "Orphanage",
       },
-      imgURL: `${process.env.PUBLIC_URL}/organizationImages/Resala.png`,
+      imgURL: `${process.env.PUBLIC_URL}/organizationImages/AwladyOrphanage.png`,
     },
     {
-      title: "Resala",
-      subtitle: "Charity",
+      title: "Church of Archangel Michael",
+      subtitle: "Church",
       description:
-        "Resala is a charitable organization dedicated to helping those in need through various initiatives.",
+        "The Church of Archangel Michael is a place of worship and spiritual refuge located in Heliopolis, Cairo. It serves as a center for the Christian community, offering regular religious services, ceremonies, and events. With its rich history and architectural beauty, the church provides a sacred space for prayer, contemplation, and community gatherings. The Church of Archangel Michael welcomes individuals from all walks of life to come together in faith and fellowship.",
       tags: {
         governorate: "Cairo",
         area: "Heliopolis",
-        type: "Charity",
+        type: "Church",
       },
-      imgURL: `${process.env.PUBLIC_URL}/organizationImages/Resala.png`,
+      imgURL: `${process.env.PUBLIC_URL}/organizationImages/MichaelChurch.png`,
+    },
+    {
+      title: "Al-Azhar Mosque",
+      subtitle: "Mosque",
+      description:
+        "The Al-Azhar Mosque, also known as the Mosque of the Most Resplendent, is one of the most important religious and educational institutions in the Islamic world. Located in Cairo, Egypt, it serves as a center for Islamic learning, theology, and jurisprudence. Founded in the 10th century, the mosque has a rich history and architectural significance, featuring stunning domes, minarets, and intricate designs. Al-Azhar Mosque hosts daily prayers, religious lectures, and academic activities, attracting visitors and students from around the globe. As a symbol of Islamic heritage and culture, the mosque plays a vital role in promoting tolerance, unity, and understanding among people of different faiths.",
+      tags: {
+        governorate: "Cairo",
+        area: "Islamic Cairo",
+        type: "Mosque",
+      },
+      imgURL: `${process.env.PUBLIC_URL}/organizationImages/AlAzhar.jpeg`,
     },
   ]);
   const [filteredOrganizations, setFilteredOrganizations] = useState([]);
