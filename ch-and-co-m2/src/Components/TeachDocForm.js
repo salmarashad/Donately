ba0ecc7b6c4bf303f336d87ab2de9998b8b5c109
Farah065 <farahahmad065@gmail.com>
2024-05-08 10:13:00 +0300

    added confirmation message to teacher/doctor verification from profile

--- ch-and-co-m2/src/Components/TeachDocForm.js
+++ ch-and-co-m2/src/Components/TeachDocForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Counter from "./Counter";
 import {ReactComponent as SpinnerSVG} from '../SVGs/spinner.svg';
+import {ReactComponent as HeartSVG} from '../SVGs/tigerh-filled.svg';
 
 function TeachDocForm(props) {
     const [loading, setLoading] = useState(false);
@@ -16,7 +17,7 @@ function TeachDocForm(props) {
             await timeout(2000);
             setLoading(false);
             if(props.profile === true) {
-                props.setPage("donations");
+                props.setConfirm(true);
             }
             else {
                 props.setStep(2);
@@ -25,69 +26,83 @@ function TeachDocForm(props) {
     }
 
     return(
-        <div>
-        {props.form === "teacher" &&
+        <>
+            {props.confirm != true ?
             <div>
-                {props.profile === true && <h2 className="text-xl text-center font-semibold mb-2">Teacher Verification Form</h2>}
-                <h2 className="text-lg font-semibold mb-1">Job Information</h2>
-                <label className='label'>Subject
-                    <input type="text" value={props.teacherData.subject} placeholder="Type here..." className="text-input"
-                        onChange={(e) => props.handleTeacherChange("subject", e.target.value)} />
-                </label>
-                <label className='label'>Number of pro-bono classes you can teach a month
-                    <Counter val={props.teacherData.numCases} setter={props.handleTeacherChange} valName="numCases" />
-                </label>
-            </div>
-        }
-        {props.form === "doctor" &&
-            <div>
-                {props.profile === true && <h2 className="text-xl text-center font-semibold mb-2">Doctor Verification Form</h2>}
-                <h2 className="text-lg font-semibold mb-1">Job Information</h2>
-                <label className='label'>Specialty
-                    <input type="text" value={props.doctorData.specialty} placeholder="Type here..." className="text-input"
-                        onChange={(e) => props.handleDoctorChange("specialty", e.target.value)} />
-                </label>
-                <label className='label'>Number of pro-bono cases you can take a month
-                    <Counter val={props.doctorData.numCases} setter={props.handleDoctorChange} valName="numCases" />
-                </label>
-
-                <hr className='border-t-2 my-4' />
-                <h2 className="text-lg font-semibold mb-1">Clinic Location</h2>
-                <label className='label'>Address
-                    <input type="text" value={props.doctorData.address} placeholder="Type here..." className="text-input"
-                    onChange={(e) => props.handleDoctorChange("address", e.target.value)} />
-                </label>
-                <div className="grid grid-cols-2 gap-x-4">
-                    <label className='label'>Area
-                        <input type="text" value={props.doctorData.area} placeholder="Type here..." className="text-input"
-                        onChange={(e) => props.handleDoctorChange("area", e.target.value)} />
+                {props.form === "teacher" &&
+                <div>
+                    {props.profile === true && <h2 className="text-xl text-center font-semibold mb-2">Teacher Verification Form</h2>}
+                    <h2 className="text-lg font-semibold mb-1">Job Information</h2>
+                    <label className='label'>Subject
+                        <input type="text" value={props.teacherData.subject} placeholder="Type here..." className="text-input"
+                            onChange={(e) => props.handleTeacherChange("subject", e.target.value)} />
                     </label>
-                    <label className='label'>Governorate
-                        <input type="text" value={props.doctorData.governorate} placeholder="Type here..." className="text-input"
-                        onChange={(e) => props.handleDoctorChange("governorate", e.target.value)} />
+                    <label className='label'>Number of pro-bono classes you can teach a month
+                        <Counter val={props.teacherData.numCases} setter={props.handleTeacherChange} valName="numCases" />
                     </label>
                 </div>
-            </div>
-        }
-        <hr className='border-t-2 my-4' />
-        <h2 className="text-lg font-semibold mb-1">Qualifications</h2>
-        <label className='label' htmlFor="pdf">CV
-            <input id="pdf" type="file" name="pdf" accept="application/pdf" className="block w-max cursor-pointer mt-1 mb-3" 
-            onChange={props.handleFileChange}/>
-        </label>
+                }
+                {props.form === "doctor" &&
+                <div>
+                    {props.profile === true && <h2 className="text-xl text-center font-semibold mb-2">Doctor Verification Form</h2>}
+                    <h2 className="text-lg font-semibold mb-1">Job Information</h2>
+                    <label className='label'>Specialty
+                        <input type="text" value={props.doctorData.specialty} placeholder="Type here..." className="text-input"
+                            onChange={(e) => props.handleDoctorChange("specialty", e.target.value)} />
+                    </label>
+                    <label className='label'>Number of pro-bono cases you can take a month
+                        <Counter val={props.doctorData.numCases} setter={props.handleDoctorChange} valName="numCases" />
+                    </label>
 
-        <div className="flex justify-center">
-            <button
-                type="submit"
-                className='submit-btn w-[80px] h-[32px]'
-                onClick={handleSubmit}
-                disabled={(props.form === "teacher" && (Object.values(props.teacherData).includes("") || props.teacherData.numCases === 0))
-                            || (props.form === "doctor" && (Object.values(props.doctorData).includes("") || props.doctorData.numCases === 0))
-                            || props.file === undefined}>
-                {loading ? <SpinnerSVG className="w-full"/> : "Submit"}
-            </button>
-        </div>
-        </div>
+                    <hr className='border-t-2 my-4' />
+                    <h2 className="text-lg font-semibold mb-1">Clinic Location</h2>
+                    <label className='label'>Address
+                        <input type="text" value={props.doctorData.address} placeholder="Type here..." className="text-input"
+                        onChange={(e) => props.handleDoctorChange("address", e.target.value)} />
+                    </label>
+                    <div className="grid grid-cols-2 gap-x-4">
+                        <label className='label'>Area
+                            <input type="text" value={props.doctorData.area} placeholder="Type here..." className="text-input"
+                            onChange={(e) => props.handleDoctorChange("area", e.target.value)} />
+                        </label>
+                        <label className='label'>Governorate
+                            <input type="text" value={props.doctorData.governorate} placeholder="Type here..." className="text-input"
+                            onChange={(e) => props.handleDoctorChange("governorate", e.target.value)} />
+                        </label>
+                    </div>
+                </div>
+                }
+                <hr className='border-t-2 my-4' />
+                <h2 className="text-lg font-semibold mb-1">Qualifications</h2>
+                <label className='label' htmlFor="pdf">CV
+                    <input id="pdf" type="file" name="pdf" accept="application/pdf" className="block w-max cursor-pointer mt-1 mb-3" 
+                    onChange={props.handleFileChange}/>
+                </label>
+
+                <div className="flex justify-center">
+                    <button
+                        type="submit"
+                        className='submit-btn w-[80px] h-[32px]'
+                        onClick={handleSubmit}
+                        disabled={(props.form === "teacher" && (Object.values(props.teacherData).includes("") || props.teacherData.numCases === 0))
+                                    || (props.form === "doctor" && (Object.values(props.doctorData).includes("") || props.doctorData.numCases === 0))
+                                    || props.file === undefined}>
+                        {loading ? <SpinnerSVG className="w-full"/> : "Submit"}
+                    </button>
+                </div>
+            </div>
+            :
+            <div className="flex flex-col items-center gap-4 text-center">
+                <h2 className="text-2xl font-bold">Your request has successfully<br/>been submitted!</h2>
+                <HeartSVG className="w-60" />
+                <p className="font-medium">You may use the website as a regular donor until your request is verified. We'll get back to you in 1-2 weeks.</p>
+                <p className="font-medium">Thank you for using donate<span className="font-sans">لي</span></p>
+                <p className="underline cursor-pointer -mt-4 italic ml-1" onClick={() => props.setPage("donations")}>
+                    Continue browsing
+                </p>
+            </div>
+            }
+        </>
     );
 }
 export default TeachDocForm;
--- ch-and-co-m2/src/Components/TeachDocPage.js
+++ ch-and-co-m2/src/Components/TeachDocPage.js
@@ -3,6 +3,7 @@ import TeachDocForm from "./TeachDocForm";
 
 function TeachDocPage(props) {
     const [form, setForm] = useState("teacher");
+    const [confirm, setConfirm] = useState(false);
 
     const [teacherData, setTeacherData] = useState({
         subject: "",
@@ -43,7 +44,7 @@ function TeachDocPage(props) {
     return(
         <div className='h-max flex flex-col items-center'>
             <div className='flex flex-col items-start w-[500px] gap-2'>
-                <div className="w-full flex gap-2 text-farahgreen-700">
+                {!confirm && <div className="w-full flex gap-2 text-farahgreen-700">
                     <button 
                         className={form === "teacher" ? "text-base rounded-md border-2 w-1/2 py-1 font-semibold border-farahgreen-400 bg-farahgreen-300"
                                                     : "text-base rounded-md border-2 w-1/2 py-1 font-semibold border-farahgreen-300 bg-farahgray-100 hover:bg-farahgreen-200"}
@@ -56,11 +57,12 @@ function TeachDocPage(props) {
                         onClick={handleFormChange}>
                         Doctor
                     </button>
-                </div>
+                </div>}
                 <div className="flex flex-col justify-center bg-white rounded-md w-full p-6 shadow-md gap-4">
                     <form className='flex flex-col w-full'>
-                        <TeachDocForm doctorData={doctorData} handleDoctorChange={handleDoctorChange} teacherData={teacherData} handleTeacherChange={handleTeacherChange} form={form}
-                        handleFileChange={handleFileChange} file={file} profile={true} setPage={props.setPage} />
+                        <TeachDocForm doctorData={doctorData} handleDoctorChange={handleDoctorChange} teacherData={teacherData}
+                        handleTeacherChange={handleTeacherChange} form={form} handleFileChange={handleFileChange} file={file}
+                        profile={true} setPage={props.setPage} confirm={confirm} setConfirm={setConfirm} />
                     </form>
                 </div>
             </div>
