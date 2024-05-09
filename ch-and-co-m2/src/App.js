9c799656c788821aea2e8d443c4e1ed57876a1f5
Ziad ElGendy <ziad.m.elgendy@gmail.com>
2024-05-09 15:34:44 +0300

    genericize postsview

--- ch-and-co-m2/src/App.js
+++ ch-and-co-m2/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import { useState, createContext } from "react";
-import DonationsView from './Views/DonationsView';
+import PostsView from './Views/PostsView.js';
 import NavbarComponent from './Components/NavbarComponent';
 import Registration from './Components/Registration';
 import Profile from './Components/Profile.js';
 import Login from './Components/Login'
 import OrganizationsView from './Views/OrganizationsView';
-import VolunteeringView from './Views/VolunteeringView';
 import DetailsView from './Components/DetailsView.js';
 import DonationForm from './Components/DonationForm.js';
 import VolunteeringForm from './Components/VolunteeringForm.js';
@@ -25,30 +24,42 @@ function App() {
 	const [data, setData] = useState({});
 
   	return (
-		<DataContext.Provider value={{ data, setData }}>
-			<DetailedContext.Provider value={{ isDetailedView, setIsDetailedView }}>
-				<UserTypeContext.Provider value={{ userType, setUserType }}>
-					<div className="h-screen font-karla text-farahgray-900">
-						{page !== "login" && page !== "registration" &&
-							<NavbarComponent setPage={setPage} isLoggedin={isLoggedin} setIsLoggedin={setIsLoggedin} />}
-						{/* rest of the body */}
-						<div className="bg-farahgray-100 h-max pb-8 pt-24 min-h-full">
-							{page === "donations" && <DonationsView/>}
-							{page === "volunteering" && <VolunteeringView />}
-							{page === "organizations" && <OrganizationsView />}
-							{page === "registration" && <Registration setPage={setPage} />}
-							{page === "profile" && <Profile setPage={setPage} />}
-							{page === "teachdocform" && <TeachDocPage setPage={setPage} />}
-							{page === "login" && <Login setIsLoggedin={setIsLoggedin} setPage={setPage} />}
-							{page === "donationForm" && <DonationForm setPage={setPage} />}
-							{page === "volunteeringForm" && <VolunteeringForm setPage={setPage} />}
+			<DataContext.Provider value={{ data, setData }}>
+				<DetailedContext.Provider value={{ isDetailedView, setIsDetailedView }}>
+					<UserTypeContext.Provider value={{ userType, setUserType }}>
+						<div className="h-screen font-karla text-farahgray-900">
+							{page !== "login" && page !== "registration" && (
+								<NavbarComponent
+									setPage={setPage}
+									isLoggedin={isLoggedin}
+									setIsLoggedin={setIsLoggedin}
+								/>
+							)}
+							{/* rest of the body */}
+							<div className="bg-farahgray-100 h-max pb-8 pt-24 min-h-full">
+								{page === "donations" && <PostsView page={page} />}
+								{page === "volunteering" && <PostsView page={page} />}
+								{page === "organizationPosts" && <PostsView page={page} />}
+								{page === "organizations" && <OrganizationsView />}
+								{page === "registration" && <Registration setPage={setPage} />}
+								{page === "profile" && <Profile setPage={setPage} />}
+								{page === "teachdocform" && <TeachDocPage setPage={setPage} />}
+								{page === "login" && (<Login setIsLoggedin={setIsLoggedin} setPage={setPage} />)}
+								{page === "donationForm" && <DonationForm setPage={setPage} />}
+								{page === "volunteeringForm" && (<VolunteeringForm setPage={setPage} />)}
+							</div>
+							{isDetailedView && (
+								<DetailsView
+									page={page}
+									setPage={setPage}
+									isLoggedin={isLoggedin}
+								/>
+							)}
 						</div>
-						{isDetailedView && <DetailsView page={page} setPage={setPage} isLoggedin={isLoggedin}/>}
-					</div>
-				</UserTypeContext.Provider>
-			</DetailedContext.Provider>
-		</DataContext.Provider>
-	);
+					</UserTypeContext.Provider>
+				</DetailedContext.Provider>
+			</DataContext.Provider>
+		);
 }
 
 export {App, DetailedContext, UserTypeContext, DataContext};
