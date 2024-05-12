770d89276966beb1aef3a53877cbd88b0ddb91dd
salmarashad <salmamarashad@gmail.com>
2024-05-12 12:34:23 +0300

    updated organization filters

--- ch-and-co-m2/src/Views/OrganizationsView.js
+++ ch-and-co-m2/src/Views/OrganizationsView.js
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
 import OrganizationCardRenderer from "../Components/OrganizationCardRenderer.js";
 import organizationData from "../DummyData/organizationsData.js";
-import Filter from "../Components/Filter.js";
+import Filter2 from "../Components/OrganizationFilter.js";
 
 const OrganizationsView = (props) => {
-	const [currentCardSet, setCurrentCardSet] = useState([]);
+  const [currentCardSet, setCurrentCardSet] = useState(organizationData); // Initialize with all data
 
-	return (
-		<div className="grid grid-cols-4 max-w-7xl w-full mx-auto">
-			<div className="col-span-1 justify-self-start">
-				<div className="flex flex-col items-center justify-center gap-4 sticky top-24">
-					<Filter
-						data={organizationData}
-						setCurrentCardSet={setCurrentCardSet}
-					/>
-				</div>
-			</div>
-			<div className="col-span-2 mx-4">
-				<div className="grid grid-cols-3 place-items-center gap-y-6 auto-rows-fr">
-					<OrganizationCardRenderer
-						userType={props.userType}
-						currentCardSet={currentCardSet}
-						setCurrentCardSet={setCurrentCardSet}
-					/>
-				</div>
-			</div>
-			<div className="col-span-1">
-				{/* Add any content you want to display in the third column here */}
-			</div>
-		</div>
-	);
+  return (
+    <div className="grid grid-cols-4 max-w-7xl w-full mx-auto">
+      <div className="col-span-1 justify-self-start">
+        <div className="flex flex-col items-center justify-center gap-4 sticky top-24">
+          <Filter2
+            data={organizationData}
+            setCurrentCardSet={setCurrentCardSet}
+          />
+        </div>
+      </div>
+      <div className="col-span-2 mx-4">
+        <div className="grid grid-cols-3 place-items-center gap-y-6 auto-rows-fr">
+          <OrganizationCardRenderer
+            userType={props.userType}
+            currentCardSet={currentCardSet} // Pass filtered data
+            setCurrentCardSet={setCurrentCardSet} // This may not be necessary here, depending on your implementation in OrganizationCardRenderer
+          />
+        </div>
+      </div>
+      <div className="col-span-1">
+        {/* Add any content you want to display in the third column here */}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationsView;
